5d6e7cf56c40e12d264b79385394ba406ac958c8
i.maulana.azn@gmail.com <i.maulana.azn@gmail.com>
2022-05-04 09:25:50 +0700

    menyelesaikan halaman list surah juz page

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,4 +1,18 @@
+const idk = (function(){
+  window.addEventListener('scroll',(function(){
+    let winTop = window.pageYOffset;
+    if(winTop >= 30){
+      
+    }else{
+    	
+    }//if-else
+  }));//win func.
+});//ready func.
+
+idk()
+
 const sortbyTitle = document.querySelectorAll('.sortby .sortby-title h3');
+
 sortbyTitle.forEach(title => title.addEventListener('mouseover',function(){
   title.classList.add('underscore')
 }
@@ -8,16 +22,19 @@ sortbyTitle.forEach(title => title.addEventListener('mouseout',function(){
 }
   ))
 
-fetch('http://api.alquran.cloud/v1/surah')
+let listContainer = document.querySelector('.sort-list')
+
+function showDefault(){
+  listContainer.innerHTML = '';
+  fetch('http://api.alquran.cloud/v1/surah')
 	.then(res => res.json())
 	.then(res => {
-		const listContainer = document.querySelector('.surah-list')
 		res.data.forEach(surah => 
-		listContainer.innerHTML += getListTemplate(surah.number,surah.englishName,surah.revelationType,surah.numberOfAyahs,surah.name)
+		listContainer.innerHTML += showSurah(surah.number,surah.englishName,surah.revelationType,surah.numberOfAyahs,surah.name)
 		)
 	})
 
-function getListTemplate(num,engName,revel,verse,arab){
+function showSurah(num,engName,revel,verse,arab){
 	return ` 
 	<a href="#">
 	  <li>
@@ -35,17 +52,73 @@ function getListTemplate(num,engName,revel,verse,arab){
 	    </div>
 	  </li>
   	</a>`
-}
+}}
 
-const idk = (function(){
-  window.addEventListener('scroll',(function(){
-    let winTop = window.pageYOffset;
-    if(winTop >= 30){
-      console.log(winTop)
-    }else{
-    	console.log(winTop)
-    }//if-else
-  }));//win func.
-});//ready func.
+showDefault()
 
-idk()
+const sortBySurah = document.getElementById('surah-btn')
+sortBySurah.addEventListener('click',showDefault)
+
+const sortByPara = document.getElementById('para-btn')
+sortByPara.addEventListener('click', function(){
+  
+  listContainer.innerHTML = '';
+  
+  fetch('http://api.alquran.cloud/v1/meta')
+    .then(data => data.json())
+    .then(data => {
+      let datas = data.data
+      datas.juzs.references.forEach((juz,i) => {
+       listContainer.innerHTML += showPara(i+1,datas.surahs.references[datas.juzs.references[i].surah-1].englishName,datas.juzs.references[i].ayah)
+      })
+    })
+  
+})
+
+function showPara(num, surah, ayah) {
+  	return ` 
+  	<a href="#">
+  	  <li>
+  	    <div class="num">
+  	      <h5>${num}</h5>
+  	    </div>
+
+  	    <div class="surah">
+  	        <h5>Juz ${num}</h5>
+  	        <p>Start from: ${surah} Ayah ${ayah}</p>
+  	    </div>
+  	  </li>
+    	</a>`
+  }
+
+const sortByPage = document.getElementById('page-btn')
+sortByPage.addEventListener('click', function(){
+  
+  listContainer.innerHTML = '';
+  
+  fetch('http://api.alquran.cloud/v1/meta')
+    .then(data => data.json())
+    .then(data => {
+      let datas = data.data
+      datas.pages.references.forEach((page,i) => {
+       listContainer.innerHTML += showPages(i+1,datas.surahs.references[datas.pages.references[i].surah-1].englishName,datas.pages.references[i].ayah)
+      })
+    })
+  
+})
+
+function showPages(num, surah, ayah) {
+  	return ` 
+  	<a href="#">
+  	  <li>
+  	    <div class="num">
+  	      <h5>${num}</h5>
+  	    </div>
+
+  	    <div class="surah">
+  	        <h5>Page ${num}</h5>
+  	        <p>Start from: ${surah} Ayah ${ayah}</p>
+  	    </div>
+  	  </li>
+    	</a>`
+  }
